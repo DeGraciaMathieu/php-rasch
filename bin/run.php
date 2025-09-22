d5634b70283dacfd5e4c0d18df106c0ff245ca3e
DeGraciaMathieu <degracia.mathieu@gmail.com>
2025-09-22 17:34:37 +0200

    Adds initial project structure
    Sets up the basic project structure with composer, autoloading, and a simple Rasch class.
    
    This commit initializes the project with:
    - A composer.json file defining the project's autoloading configuration and development dependencies.
    - A composer.lock file to lock dependencies.
    - A .gitignore file to exclude the vendor directory from version control.
    - A basic Rasch class in the src/ directory.
    - A run.php script in the bin/ directory to instantiate and call the Rasch class.

---
+++ bin/run.php
@@ -0,0 +1,6 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$rasch = new App\Rasch();
+$rasch->calc();
---
+++ src/Rasch.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Rasch
+{
+    public function calc()
+    {
+        //
+    }
+}
